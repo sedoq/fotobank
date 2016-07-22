5859e5775f5566bdc80cdb62269e28635ee35e03
Andrey G <sedoq@mail.ru>
2016-07-22 18:05:14 +0300

    some work

---
+++ catalog.config.inc.php
@@ -0,0 +1,16 @@
+<?php
+$settings['display'] = 'vertical';
+$settings['fields'] = array(
+    'title' => array(
+        'caption' => 'Заголовок',
+        'type' => 'text'
+    ),
+    'count' => array(
+        'caption' => 'Количество',
+        'type' => 'text'
+    ),
+	 'file' => array(
+        'caption' => 'Файл',
+        'type' => 'file'
+    ),
+	);
---
+++ decor.config.inc.php
@@ -0,0 +1,21 @@
+<?php
+$settings['display'] = 'vertical';
+$settings['fields'] = array(
+    'title' => array(
+        'caption' => 'Текст',
+        'type' => 'text'
+    ),
+	'img' => array(
+        'caption' => 'Картинка',
+        'type' => 'image'
+    ),
+    'thumb' => array(
+        'caption' => 'Thumbnail',
+        'type' => 'thumb',
+        'thumbof' => 'img'
+    ),
+    'url' => array(
+        'caption' => 'Ссылка',
+        'type' => 'text'
+    )
+);
