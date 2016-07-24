695ff7c8bb8f7096cfe8e4e5d5f23f7786ee886a
Andrew G <sedoq@mail.ru>
2016-07-24 21:44:31 +0300

    Some work in gallery

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,35 @@
-$
+$(document).ready(function(){
+    $(".menu > label").on("click", function(){
+        $(".menu > label.current-sort").removeAttr("class");
+        
+        $(this).addClass("current-sort");
+    });
+    
+     $(".colors-radio > label").on("click", function(){
+        $(".colors-radio > label.current").removeAttr("class");
+        
+        var color = $(this).attr("style");
+        
+        $(".current-color").attr("style", color); 
+        $(this).addClass("current");
+        
+         
+    });
+    
+    
+    //ГАЛЕРЕЯ
+    var load_variant = $(".menu > #current_load");
+    
+    //Actions
+    
+    
+    $(".menu > input[type='radio']").on("click", function(){
+        $(load_variant).val($(this).val());
+    });
+    
+    var query = {
+        'load': $(load_variant).val(),
+    } 
+    
+    console.log(query.load);
+});
